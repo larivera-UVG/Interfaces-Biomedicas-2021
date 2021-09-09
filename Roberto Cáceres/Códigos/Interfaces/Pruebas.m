dc1bc49f3b74572aedaaae97f846b280ffe17fdf
Cac17163 <cac17163@uvg.edu.gt>
2021-09-08 20:37:45 -0600

    Interfaces
    Interfaz EMG NItida

--- Roberto Cáceres/Códigos/Interfaces/Pruebas.m
+++ Roberto Cáceres/Códigos/Interfaces/Pruebas.m
@@ -1,95 +1,66 @@
-%% Pruebas R17
+% Create the object
+m = 5;      %No. muestras totales
+% Start background acquisition
 
-% 1. Primero llevar a su posición de Home (extender el brazo verticalmente).
-
-%% Inicialización
-% 2. Inicializar el R17, se debe indicar el puerto COM y su velocidad.
-PuertoCOM = 'COM3';
-speed = 1000;
-R17 = R17Init(PuertoCOM, speed);
-
-%% Cinemática directa
-% 3. Probar una configuración (cinemática directa)
-% Se ingresan los valores de configuración para las revolutas en grados.
-
-% Rango entre -0.6 y 0.6
-career = 0;
-% Rango entre -135 y 135 (anterior)
-% Rango entre -180 y 180
-waist =  100;
-% Rango entre -111 y 111 (anterior)
-% Rango entre -142 y 142
-shouler =  0;
-% Rango entre -150 y 150 (anterior)
-% Rango entre -163 y 163
-elbow =  0;
-% Rango entre -180 y 180
-hand =  0;
-% Rango entre -180 y 180
-wrist =  160;
-
-q = [career; deg2rad(waist); deg2rad(shouler); deg2rad(elbow);...
-     deg2rad(hand); deg2rad(wrist)];
-
-R17fKine(R17, q);
+b = Bitalino;
 
 
-%% Desconexión
-% 4. Desconectar el R17.
-R17Close(R17);
+time = 0;
+b.startBackground;
 
-%% Otros comandos útiles
-% R17Command(R17, '');
+% Pause to acquire data for 20 seconds
+%pause(10);
+% Read the data from the device
 
-command = 1;
+'HELLOO'
 
-switch command
-    case 1
-        R17Command(R17, 'SPEED ?');
-    case 2
-        R17Command(R17, 'ACCEL ?');
-    case 3
-        R17Command(R17, '2000 SPEED !');
-    case 4
-        R17Command(R17, '2000 ACCEL !');
-    case 5
-        R17Command(R17, 'HOME');
-    case 6
-        R17Command(R17, 'CARTESIAN');
-    case 7
-        R17Command(R17, 'WHERE');
-    case 8
-        R17Command(R17, 'COMPUTE CARTWHERE');
-    case 9
-        R17Command(R17, 'WHEREIS PART');
-    case 10
-        R17Command(R17, 'HOLDING PART');
-    case 11
-        R17Command(R17, 'TRACKSPEED .');
-    case 12
-        R17Command(R17, 'ROLLSPEED .');
-        case 12
-        R17Command(R17, 'VIEW WORKSPACE');
-end
+%data = readCurrentValues(b, 5000)*5/255;
 
+                while (time < m)
+                    pause(1)
+                    time = time + 1
+                end
 
-% R17Command(R17, 'VIEW LIMITS');
-% R17Command(R17, 'B-RATIO .'); %WAIST ratio
-% R17Command(R17, 'S-RATIO .'); %SHOULDER ratio
+% Read the data from the device
+data = b.read*5/1024;       
+%hola =          data(:,2)
+% Stop background acquisition of data
+b.stopBackground;
 
-%%
-xlim([-500 500]);
-ylim([-500 500]);
-zlim([-500 500]);
-plot3(-500:500,0,0);
-hold on;
-line(2*xlim, [0,0], [0,0], 'LineWidth', 3, 'Color', 'k');
+% Clean up the bitalino object
+delete(b)
+F_pb = filtro_pasa_banda(1000,20,450);    %DiseÃ±ar filtro pasa banda       
+F_notch = filtro_rechaza_banda(1000);     %DiseÃ±ar filtro rechaza banda 
+data_f = filter(F_pb, data(:,6)');                          %Aplicar filtro pasa bandas
+data_n = filter(F_notch, data_f);                       %Aplicar filtro notch
+
+ Epocas = 10100
+ L = Epocas;
+ t=linspace(0, Epocas/1000, L);
+ h1 = plot( t, data_n)
+ ylim([min(data_n) max(data_n)]);
+h1.YData = data_n
+
+Data.WaveC5EMG(2) = jEnhancedWaveLength(data_n);
+Data.WaveFormC5EMG(2)= jWaveformLength(data_n);
+Data.MaximumFC5EMG(2) = jMaximumFractalLength(data_n);
+Data.SkewnessFC5EMG(2) = jSkewness(data_n);
+%[year month day hour minute seconds
+
+yfit = TREE_Canal5EMG.predictFcn(Data)
+switch yfit
+    case 'Left'
+        %q2 = [0; deg2rad(60);0; 0;deg2rad(90); 0];
+        
+        %R17fKine(R17, q2);
+        'hola'
+    case 'Forward'
+        %q2 = [0; deg2rad(60);0; deg2rad(45);deg2rad(90); 0];
+        %R17fKine(R17, q2);
+        'hola2'
+    end
 
 
 
-
-
-
-
-
+        
 
