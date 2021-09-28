7f9609d329d7dd3d6dc0753f43d14b7fc2c27270
Cac17163 <cac17163@uvg.edu.gt>
2021-09-28 09:56:40 -0600

    ActualizacionFinalOctubre
    Interfaz EMG de recoleccion muy afinada

--- Roberto Cáceres/Códigos/Interfaces/DataContinuaOtraForma.m
+++ Roberto Cáceres/Códigos/Interfaces/DataContinuaOtraForma.m
@@ -2,16 +2,17 @@
 %% codigo para adquirir data continua desde el bitalino. 
 
 TiempoBuffer = 5;      %No. muestras totales
-b = Bitalino;
+%b = Bitalino;
 time = 0;
  n=0;
  b_act = 0 ;
- volt = 0; 
+ M = 0; 
+ volt = 0 ;
  %esta funcion prepara al bitalino para empezar funcionar en modo streaming
  %y no dejara de enviar hasta que se haga un b.stopBackground mientras
  %tanto se empieza a recibir lo datos de acuerdo a como se lleno el bufer. 
 
-
+data_n=0;
 
 
 %se preparan los filtros 
@@ -19,6 +20,8 @@ F_pb = filtro_pasa_banda(1000,20,450);    %Diseñar filtro pasa banda
 F_notch = filtro_rechaza_banda(1000);     %Diseñar filtro rechaza banda 
 th  = 0.20
 ga = 0.15
+i=0;
+j=0;
 b.startBackground
 'HELLOO'
 while 1
@@ -27,55 +30,60 @@ while 1
                     pause(1)   % se le da una pausa de 1 segundo por cada vez que pase, para que se logre llenar el buffer en el tiempo que se requiere
                     time = time + 1 %variable que aumenta cada 1 segundo. 
    end
-    data = b.read;% tomamos los ultimos valores en el buffer.              
+   
+    data = read(b);% tomamos los ultimos valores en el buffer.              
     %n=n+1%contador para ingresar los datos a la tabla. 
     time = 0; %se reinicia el contador para la siguiente vez que se llene el buffer
     EMG_raw = data(:,6); %se toma solo la columna de la senal EMG
     EMG = EMG_raw*(3.3/1024);%se convierte la senal a la escala de 3.3 V
     data_f = filter(F_pb, EMG');                          %Aplicar filtro pasa bandas
     data_n = filter(F_notch, data_f);                       %Aplicar filtro notch
-    volt = max(data_n)
+    wt = cwt(data_n);
+   volt = max(data_n);
     
-    %if  volt > (th + ga) &&  b_act == 0  && volt < 5 %este th se determina con pruebas previas. 
+    M = max(max(wt));
+    Th = abs(M)
+    
+    if  Th > 0.31  &&  b_act == 0  && volt < 5 %este th se determina con pruebas previas. 
             %Activar bandera de inicio de actividad
-       %     b_act = 1;
-    %end
+            b_act = 1;
+    end
         
-    
+    n = n+1;
     
      
     
-    %if b_act ==1
-      %b_act = 0 ;  
+    if b_act ==1
+        Epocas = size(data_n,2) ; %se toma el tamano del buffer obtenido
+    L = Epocas; %longitud del vector epocas
+    t=linspace(0, Epocas/1000, L);
+    h1 = plot( t, data_n) %se plotea esa muestra
+      b_act = 0 ;  
     Data.WaveC5EMG(1) = jEnhancedWaveLength(data_n);
     Data.WaveFormC5EMG(1)= jWaveformLength(data_n);
     Data.MaximumFC5EMG(1) = jMaximumFractalLength(data_n);
     Data.SkewnessFC5EMG(1) = jSkewness(data_n);
         
-        
+     
         yfit = TREE_Canal5EMG.predictFcn(Data)
-    switch yfit
-     case 'Left'
-        %q2 = [0; deg2rad(60);0; 0;deg2rad(90); 0];
-        
-        %R17fKine(R17, q2);
-        'hola'
-         case 'Forward'
-        %q2 = [0; deg2rad(60);0; deg2rad(45);deg2rad(90); 0];
-        %R17fKine(R17, q2);
-        'hola2'
-    %end
-    end 
+     switch yfit
+      case 'Left'
+          %q2 = [0; deg2rad(60);0; 0;deg2rad(90); 0];
+         
+         %R17fKine(R17, q2);
+         'hola'
+          case 'Forward'
+         %q2 = [0; deg2rad(60);0; deg2rad(45);deg2rad(90); 0];
+         %R17fKine(R17, q2);
+         'hola2'
+     end
+     end 
     
-    Epocas = size(data_n,2) ; %se toma el tamano del buffer obtenido
-    L = Epocas; %longitud del vector epocas
     
-    t=linspace(0, Epocas/1000, L);
-    h1 = plot( t, data_n) %se plotea esa muestra
     
     
 end 
-b.stopBackground;
+b.stopBackground
 
 delete(b)
 
